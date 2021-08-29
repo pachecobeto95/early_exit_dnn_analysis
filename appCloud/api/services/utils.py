8950bad937a1982c81bf4c9669dc1a543dfdcedb
Roberto Pacheco <robertopvc@gmail.com>
2021-08-29 18:27:41 -0300

    updating

--- appCloud/api/services/utils.py
+++ appCloud/api/services/utils.py
@@ -5,11 +5,44 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from PIL import Image
-from .mobilenet import B_MobileNet
+from .early_exit_dnn import Early_Exit_DNN
+import pandas as pd
 
 def load_model(device):
-	b_mobilenet_model = B_MobileNet(config.n_classes, config.pretrained, config.n_branches, config.input_shape, 
-		config.exit_type, device)
 
-	b_mobilenet_model.load_state_dict(torch.load(config.model_path, map_location=device)["model_state_dict"])	
-	return b_mobilenet_model.to(device)
+	early_exit_model = Early_Exit_DNN(config.model_name, config.n_classes, config.pretrained, config.nr_branch_model, 
+		config.input_shape, config.exit_type, device, distribution=config.distribution)
+
+
+	early_exit_model.load_state_dict(torch.load(config.model_path_edge_final, map_location=device)["model_state_dict"])	
+	return early_exit_model.to(device)
+
+class ModelLoad():
+	def __init__(self):
+		self._nr_branches_model = None
+
+	@property
+	def nr_branches_model(self):
+		return self._nr_branches_model
+
+
+	@nr_branches_model.setter
+	def nr_branches_model(self, nr_branches):
+		self._nr_branches_model = nr_branches
+
+
+	def update_load_model(self, device, dataset_name):
+
+		self.b_model = Early_Exit_DNN(config.model_name, config.n_classes, config.pretrained, self.nr_branches_model, 
+			config.input_shape, config.exit_type, device, distribution=config.distribution)
+
+		model_path = os.path.join(config.cloud_model_path, "mobilenet_%s_2_branches_%s.pth"%(dataset_name, self.nr_branches_model))
+		self.b_model.load_state_dict(torch.load(model_path, map_location=device)["model_state_dict"])	
+
+	def load_temperature(self):
+		temp_path = os.path.join(config.temp_cloud_path, "branches_temp_scaling_branches_%s_2.csv"%(self._nr_branches_model))
+		df_temp = pd.read_csv(temp_path)
+		df_temp = df_temp.loc[:, ~df_temp.columns.str.contains('^Unnamed')]
+		df_temp = df_temp[df_temp.p_tar==0.8]
+		temp_list = [nn.Parameter(torch.tensor([df_temp["temperature_branch_%s"%(i)].unique().item()])) for i in range(1, self._nr_branches_model+1)]
+		self.b_model.temperature_branches = temp_list
